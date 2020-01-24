29351aa31e0e3e7c53514b3e735175b7b2dced33
Shaina C <scailles23@gmail.com>
2020-01-24 23:51:19 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,58 @@
+class TicTacToe
+  
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+          ]
+  
+  
+  def initialize
+  @board=Array.new(9," ")
+  end
+
+  def display_board
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input)
+    index=input.to_i - 1
+  end
+    
+  def move(index, token="X")
+    @board[index]=token
+  end
+  
+  def position_taken?(index)
+    !((@board[index].nil?) || (@board[index] == " "))
+  end
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
+def turn
+  puts "Please choose a number 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    player_token = current_player
+    move(index, player_token)
+    display_board
+  else
+    turn
+  end
+end
+    
+  
+  
+end
